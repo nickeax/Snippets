7f9b756c8dd3082e9faca0838574720236e23687
Nick Fletcher <nickeax@gmail.com>
2021-09-05 12:56:12 +1000

    Add two actions to HelloWorldController

---
+++ Snippets/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Snippets.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World";
+        }
+
+        public string Welcome(string name = "User", int numTimes = 0)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
