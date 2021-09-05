37f2e781700da8cc7536f3a2769371f46143fd79
Nick Fletcher <nickeax@gmail.com>
2021-09-05 16:59:11 +1000

    Add some annotations.

--- Snippets/Models/Movie.cs
+++ Snippets/Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,14 @@ namespace Snippets.Models
 {
     public class Movie
     {
-		public int Id { get; set; }
-		public string Title { get; set; }
-		[DataType(DataType.Date)]
-		public DateTime ReleaseDate { get; set; }
-		public string Genre { get; set; }
-		public decimal Price { get; set; }
-	}
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+    }
 }
