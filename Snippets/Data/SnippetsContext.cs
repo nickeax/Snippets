388643a536706ec44042030db784a87c878c035a
Nick Fletcher <nickeax@gmail.com>
2021-09-05 14:06:38 +1000

    Add scaffolded MoviesController plus the CRUD Views and packages.

---
+++ Snippets/Data/SnippetsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Snippets.Models;
+
+namespace Snippets.Data
+{
+    public class SnippetsContext : DbContext
+    {
+        public SnippetsContext (DbContextOptions<SnippetsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Snippets.Models.Movie> Movie { get; set; }
+    }
+}
